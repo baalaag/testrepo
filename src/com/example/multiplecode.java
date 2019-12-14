dc05f48b68b1b43ddba1deb9875b5bb80d8e0db3
bala <baalaag28@gmail.com>
2019-12-14 16:52:09 +0530

    second commit

---
+++ src/com/example/multiplecode.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class multiplecode {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a*b;
+	System.out.println("sum of numbers"+c);
+	}
+
+}
