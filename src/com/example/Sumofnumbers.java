4a6af1393980f6f1b6b9c806e4295318eb03a50b
bala <baalaag28@gmail.com>
2019-12-14 16:41:10 +0530

    first commit

---
+++ src/com/example/Sumofnumbers.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Sumofnumbers {
+	
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=a+b;
+System.out.println("sum of numbers"+c);
+}
+
+}
